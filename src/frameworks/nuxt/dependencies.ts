864d324c9529e4f7e04318e8edf4654ddfbb8441
Marco <marco.nc.arruda@gmail.com>
2025-03-11 11:02:16 +0000

    feat: copy and paste from next folder

---
+++ src/frameworks/nuxt/dependencies.ts
@@ -0,0 +1,7 @@
+const zodVersion = "3.24.2";
+const vueRouterVersion = "^4.5.0";
+
+export const getDependenciesVersionsToInstall = () => ({
+  "vue-router": vueRouterVersion,
+  zod: zodVersion,
+});
--- src/frameworks/nuxt/index.ts
+++ src/frameworks/nuxt/index.ts
@@ -1,5 +1,95 @@
+import * as fs from "fs";
+import { generateFiles, OverwriteStrategy } from "@/lib/generate-files";
 import { Framework } from "@/types";
+import { updateJson } from "@/lib/utils";
+import { getDependenciesVersionsToInstall } from "./dependencies";
 
-export const generator = async function (framework: Framework, target: string, data: any) {
-  throw new Error('Not implemented');
-}
+export const generator = async function (
+  framework: Framework,
+  target: string,
+  options: any,
+) {
+  const templates = framework.templates;
+  const srcPath = `${target}${fs.existsSync(`${target}/src`) ? "/src" : ""}`;
+
+  options.isAppRouter =
+    fs.existsSync(`${target}/app`) || fs.existsSync(`${target}/src/app`);
+
+  if (options.isAppRouter) {
+    await generateFiles({
+      files: Object.values(templates.app),
+      target: `${srcPath}/app`,
+      substitutions: options,
+      render: framework.render,
+      options: { overwriteStrategy: OverwriteStrategy.Prompt },
+    });
+
+    await generateFiles({
+      remaps: {
+        "list.ejs": "__resources__/page.__ext__x.ejs",
+        "new.ejs": "__resources__/new/page.__ext__x.ejs",
+        "show.ejs": "__resources__/[id]/page.__ext__x.ejs",
+        "edit.ejs": "__resources__/[id]/edit/page.__ext__x.ejs",
+      },
+      files: Object.values(templates["shared/pages"]),
+      target: `${srcPath}/pages`,
+      substitutions: options,
+      render: framework.render,
+      options: { overwriteStrategy: OverwriteStrategy.Overwrite },
+    });
+  } else {
+    await generateFiles({
+      files: Object.values(templates.pages),
+      target: `${srcPath}/pages`,
+      substitutions: options,
+      render: framework.render,
+      options: { overwriteStrategy: OverwriteStrategy.Prompt },
+    });
+
+    await generateFiles({
+      remaps: {
+        "list.ejs": "__resources__/index.__ext__x.ejs",
+        "new.ejs": "__resources__/new.__ext__x.ejs",
+        "show.ejs": "__resources__/[id]/index.__ext__x.ejs",
+        "edit.ejs": "__resources__/[id]/edit.__ext__x.ejs",
+      },
+      files: Object.values(templates["shared/pages"]),
+      target: `${srcPath}/pages`,
+      substitutions: options,
+      render: framework.render,
+      options: { overwriteStrategy: OverwriteStrategy.Overwrite },
+    });
+  }
+
+  await generateFiles({
+    files: Object.values(templates["shared/components"]),
+    target: `${srcPath}/components`,
+    substitutions: options,
+    render: framework.render,
+    options: { overwriteStrategy: OverwriteStrategy.Prompt },
+  });
+
+  await generateFiles({
+    files: Object.values(templates["others"]),
+    target: srcPath,
+    substitutions: options,
+    render: framework.render,
+    options: { overwriteStrategy: OverwriteStrategy.Overwrite },
+  });
+
+  // package.json
+  updateJson(`${srcPath}/../package.json`, (json) => {
+    json.dependencies = {
+      ...json.dependencies,
+      ...getDependenciesVersionsToInstall(),
+    };
+
+    return json;
+  });
+
+  // env file
+  fs.writeFileSync(
+    `${target}/.env.local`,
+    "NEXT_PUBLIC_API_URL=http://localhost:3000/api",
+  );
+};

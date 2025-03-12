ee3eb0a4b74da02e5ae710984f46688b6693363d
Marco <marco.nc.arruda@gmail.com>
2025-03-12 01:42:36 +0000

    feat: finished nuxt templates

--- src/frameworks/nuxt/dependencies.ts
+++ src/frameworks/nuxt/dependencies.ts
@@ -1,7 +1,9 @@
-const zodVersion = "3.24.2";
+const piniaNuxtVersion = "^0.10.1";
 const vueRouterVersion = "^4.5.0";
+const zodVersion = "3.24.2";
 
 export const getDependenciesVersionsToInstall = () => ({
+  "@pinia/nuxt": piniaNuxtVersion,
   "vue-router": vueRouterVersion,
   zod: zodVersion,
 });
--- src/frameworks/nuxt/index.ts
+++ src/frameworks/nuxt/index.ts
@@ -13,55 +13,115 @@ export const generator = async function (
   const templates = framework.templates;
   const srcPath = target;
 
+  // prettier parse fn
+  const parser = (path: string) =>
+    path.endsWith(".vue") ? "vue" : "typescript";
+
   // app level files
   await generateFiles({
+    remaps: {
+      "app.vue.ejs": "app.vue",
+      "index.vue.ejs": "pages/index.vue",
+      "nuxt.config.ts.ejs": "nuxt.config.ts",
+    },
     files: Object.values(templates.app),
-    target: `${srcPath}/app`,
+    target: `${srcPath}`,
     substitutions: options,
     render: framework.render,
-    options: { overwriteStrategy: OverwriteStrategy.Prompt, parser: "vue" },
+    options: { overwriteStrategy: OverwriteStrategy.Prompt, parser },
+  });
+  await generateFiles({
+    files: Object.values(templates.layout),
+    target: `${srcPath}/layout`,
+    substitutions: options,
+    render: framework.render,
+    options: { overwriteStrategy: OverwriteStrategy.Prompt, parser },
+  });
+  await generateFiles({
+    files: Object.values(templates.types),
+    target: `${srcPath}/types`,
+    substitutions: options,
+    render: framework.render,
+    options: {
+      overwriteStrategy: OverwriteStrategy.Prompt,
+      parser,
+    },
   });
 
-  // model level files
+  // composables
+  await generateFiles({
+    files: Object.values(templates.composables),
+    target: `${srcPath}/composables`,
+    substitutions: options,
+    render: framework.render,
+    options: {
+      overwriteStrategy: OverwriteStrategy.Prompt,
+      parser,
+    },
+  });
+
+  // model store
+  await generateFiles({
+    files: Object.values(templates["stores"]),
+    target: `${srcPath}/stores`,
+    substitutions: options,
+    render: framework.render,
+    options: {
+      overwriteStrategy: OverwriteStrategy.Prompt,
+      parser,
+    },
+  });
+
+  // model service
+  await generateFiles({
+    files: Object.values(templates["services"]),
+    target: `${srcPath}/services`,
+    substitutions: options,
+    render: framework.render,
+    options: {
+      overwriteStrategy: OverwriteStrategy.Prompt,
+      parser,
+    },
+  });
+
+  // model components
+  await generateFiles({
+    remaps: {
+      "types.ts.ejs": "__resources__/types.ts",
+      "form.vue.ejs": "__resources__/form.vue",
+      "details.vue.ejs": "__resources__/details.vue",
+    },
+    files: Object.values(templates["shared/components"]),
+    target: `${srcPath}/components`,
+    substitutions: options,
+    render: framework.render,
+    options: {
+      overwriteStrategy: OverwriteStrategy.Prompt,
+      parser,
+    },
+  });
+
+  // model views
   await generateFiles({
     remaps: {
       "list.ejs": "__resources__/index.vue.ejs",
       "new.ejs": "__resources__/new.vue.ejs",
       "show.ejs": "__resources__/[id]/index.vue.ejs",
       "edit.ejs": "__resources__/[id]/edit/index.vue.ejs",
+      "helpers.ts.ejs": "__resources__/[id]/helpers.ts",
     },
     files: Object.values(templates["shared/pages"]),
     target: `${srcPath}/pages`,
     substitutions: options,
     render: framework.render,
-    options: { overwriteStrategy: OverwriteStrategy.Overwrite, parser: "vue" },
-  });
-
-  // shared components
-  await generateFiles({
-    remaps: {
-      "form.ejs": "__resources__/form.vue",
-      "details.vue.ejs": "__resources__/details.vue",
-      "types.ejs": "__resources__/types.ts",
+    options: {
+      overwriteStrategy: OverwriteStrategy.Overwrite,
+      parser: (path: string) => (path.endsWith(".vue") ? "vue" : "typescript"),
     },
-    files: Object.values(templates["shared/components"]),
-    target: `${srcPath}/components`,
-    substitutions: options,
-    render: framework.render,
-    options: { overwriteStrategy: OverwriteStrategy.Prompt, parser: "vue" },
-  });
-
-  // others
-  await generateFiles({
-    files: Object.values(templates["others"]),
-    target: srcPath,
-    substitutions: options,
-    render: framework.render,
-    options: { overwriteStrategy: OverwriteStrategy.Overwrite, parser: "vue" },
   });
 
   // package.json
-  updateJson(`${srcPath}/../package.json`, (json) => {
+  updateJson(`${srcPath}/package.json`, (json) => {
     json.dependencies = {
       ...json.dependencies,
       ...getDependenciesVersionsToInstall(),
